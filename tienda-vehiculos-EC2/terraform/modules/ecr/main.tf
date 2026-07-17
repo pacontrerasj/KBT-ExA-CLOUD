2e2651c5d2f8f24f70779a77c1b58b6a707b7759
pacontrerasj <pa.contrerasj@duocuc.cl>
2026-07-16 20:18:33 -0400

    fix: forzar le destroy de ecr

--- tienda-vehiculos-EC2/terraform/modules/ecr/main.tf
+++ tienda-vehiculos-EC2/terraform/modules/ecr/main.tf
@@ -5,6 +5,7 @@
 resource "aws_ecr_repository" "frontend" {
   name                 = "${var.proyecto}-frontend"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
@@ -16,6 +17,7 @@ resource "aws_ecr_repository" "frontend" {
 resource "aws_ecr_repository" "backend" {
   name                 = "${var.proyecto}-backend"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
